13cf156a95ade5e24c000aebbe5984a96f2ef836
chris-eichhorn <ceichhorn@gmail.com>
2024-05-08 12:22:00 -0500

    May8 updates

--- src/data/ProjectsInGithub.js
+++ src/data/ProjectsInGithub.js
@@ -10,10 +10,46 @@ const ProjectsInGithub = [
     },
 
     {
-        title: "Lorem ipsum title",
-        subtitle: "Lorem ipsum subtitle",
+        title: "TinDog webpage",
+        subtitle: "Html / CSS Project",
+        imgURL: "tindog.png",
+        description: "Html / CSS website project where the site is entirely build in html tags and css with an animated gradient background image",
+        publishLink: "https://eichhorn-personal.github.io/cge-tindog-website/",
+        repoLink: "https://github.com/Eichhorn-personal/cge-tindog-website"
+    },
+
+    {
+        title: "Drum Kit webpage",
+        subtitle: "Html / CSS / JSX Project",
+        imgURL: "drumkit.png",
+        description: "Simple html / css page that uses javascript to case/switch keyboard/click to play sounds",
+        publishLink: "https://eichhorn-personal.github.io/drum-kit-website/",
+        repoLink: "https://github.com/Eichhorn-personal/drum-kit-website"
+    },
+
+    {
+        title: "Dicee Game",
+        subtitle: "Html / CSS / JSX Project",
+        imgURL: "dicee.png",
+        description: "Simple html / css site that creates a dice game. The dice and outcomes reroll on every refresh. Page updates and dice rolls are from the js file.",
+        publishLink: "https://eichhorn-personal.github.io/cge-dicee-website/",
+        repoLink: "https://github.com/Eichhorn-personal/cge-dicee-website/settings/pages"
+    },
+  
+    {
+        title: "Sample Title",
+        subtitle: "Sample Subtitle",
         imgURL: "http://loremflickr.com/345/194",
-        description: "Sample card created using lorem ipsum text and html breaks within a JSON object string field to test rendering a string with html breaks through JSX.<br><br>Uses https://loremflickr.com/ for header image",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Pharetra diam sit amet nisl suscipit adipiscing bibendum est. Velit aliquet sagittis id consectetur purus. Dolor morbi non arcu risus quis. Justo laoreet sit amet cursus sit amet. ",
+        publishLink: "",
+        repoLink: ""
+    },
+
+    {
+        title: "Sample Title",
+        subtitle: "Sample Subtitle",
+        imgURL: "http://loremflickr.com/345/194/dog",
+        description: "Sample description",
         publishLink: "",
         repoLink: ""
     },
@@ -21,25 +57,7 @@ const ProjectsInGithub = [
     {
         title: "Lorem ipsum title",
         subtitle: "Lorem ipsum subtitle",
-        imgURL: "http://loremflickr.com/345/194",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tempus quam pellentesque nec. Pharetra diam sit amet nisl suscipit adipiscing bibendum est. Velit aliquet sagittis id consectetur purus. Dolor morbi non arcu risus quis. Justo laoreet sit amet cursus sit amet. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero id. Tempor orci dapibus ultrices in iaculis nunc sed. Amet volutpat consequat mauris nunc congue nisi vitae suscipit. Elementum curabitur vitae nunc sed velit dignissim sodales.",
-        publishLink: "",
-        repoLink: ""
-    },
-
-    {
-        title: "Lorem ipsum title",
-        subtitle: "Lorem ipsum subtitle",
-        imgURL: "http://loremflickr.com/345/194",
-        description: "",
-        publishLink: "",
-        repoLink: ""
-    },
-
-    {
-        title: "Lorem ipsum title",
-        subtitle: "Lorem ipsum subtitle",
-        imgURL: "http://loremflickr.com/345/194",
+        imgURL: "http://loremflickr.com/345/194/paris",
         description: "",
         publishLink: "",
         repoLink: ""
